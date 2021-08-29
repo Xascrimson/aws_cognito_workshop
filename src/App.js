2ccfcad2a9c7d714ac112c66c5f3c6696cf814b7
Danny Huang <dannyhuang1234@hotmail.com>
2021-08-29 23:22:11 +1200

    cognito

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {
+	Container,
+	Grid,
+	Card,Button
+} from "@material-ui/core";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <br/>
+      <Grid container justifyContent='space-around'>
+        <Card><Button>get authenticate</Button></Card>
+          <Card><Button>after authenticate</Button></Card>
+      </Grid>
+    </Container>
   );
 }
 
